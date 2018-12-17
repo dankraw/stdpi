86773872e0095597fc5de8e196f828345a25e2ce
Daniel Krawczyk <daniel.krawczyk@allegro.pl>
2018-12-17 16:37:50 +0100

    second

--- file.py
+++ file.py
@@ -1,30 +1,50 @@
 import time
 import RPi.GPIO as GPIO
+
 GPIO.setmode(GPIO.BCM)
 
-id = 17
-id2 = 27
-GPIO.setup(id, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(id2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+chan1 = 17
+chan_reset = 27
+locked = True
+
+GPIO.setup(chan1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(chan_reset, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 file_path = "/tmp/file"
 cmd = "DEPLOY HERMES!!!\n"
 
-def pressCb(param):
-	print "button %s pressed" % (param)
+def lock():
+	global locked
+	locked = True
+
+def unlock():
+	global locked
+	locked = False
+
+def button_pressed(param):
+	if locked:
+		return
+	lock()
+	print "button %s pressed" % param
 	f = open(file_path, "a")
 	f.write(cmd)
 	f.close()
 
-GPIO.add_event_detect(id, GPIO.RISING, bouncetime=1300)
-GPIO.add_event_callback(id, pressCb)
+def reset_pressed(param):
+	if not locked:
+		return
+	print "reset"
+	unlock()
 
-GPIO.add_event_detect(id2, GPIO.RISING, bouncetime=1300)
-GPIO.add_event_callback(id2, pressCb)
+GPIO.add_event_detect(chan_reset, GPIO.RISING, bouncetime=1300)
+GPIO.add_event_callback(chan_reset, reset_pressed)
+
+GPIO.add_event_detect(chan1, GPIO.RISING, bouncetime=1300)
+GPIO.add_event_callback(chan1, button_pressed)
 
 while True:
-	time.sleep(0.3) 
-	print "alive"
+	time.sleep(1)
+	print ""
 
 
 
